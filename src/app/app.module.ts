fc653d442618a8ef7fa8947e10fa9a23b2229328
Julia Kieserman <jk790u@att.com>
2017-03-30 14:50:49 -0500

    connect firebase project to angular

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,8 +2,17 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
+import { AngularFireModule } from 'angularfire2';
 
 import { AppComponent } from './app.component';
+
+export const firebaseConfig = {
+    apiKey: "AIzaSyAV6yJxkRZhdkmDcrg4DuW6PGdrxts_1As",
+    authDomain: "fresas-frescas-iot.firebaseapp.com",
+    databaseURL: "https://fresas-frescas-iot.firebaseio.com",
+    storageBucket: "fresas-frescas-iot.appspot.com",
+    messagingSenderId: "758261002536"
+};
 
 @NgModule({
   declarations: [
@@ -12,7 +21,8 @@ import { AppComponent } from './app.component';
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    AngularFireModule.initializeApp(firebaseConfig)
   ],
   providers: [],
   bootstrap: [AppComponent]
