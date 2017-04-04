2ae11dd572a534cf4570a818ffa10e190e2a2ff3
Julia Kieserman <jk790u@att.com>
2017-04-04 15:05:05 -0500

    lots of changes this is a complicated file structure

--- src/app/pages/dashboard/dashboard.component.ts
+++ src/app/pages/dashboard/dashboard.component.ts
@@ -1,13 +1,212 @@
-import {Component} from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import { AngularFire, FirebaseListObservable } from 'angularfire2';
+
 
 @Component({
   selector: 'dashboard',
   styleUrls: ['./dashboard.scss'],
   templateUrl: './dashboard.html'
 })
-export class Dashboard {
+export class Dashboard implements OnInit {
 
-  constructor() {
+  private graph1: boolean = true;
+  private graph2: boolean = false;
+  private graph3: boolean = false;
+  private _af: AngularFire;
+
+  private changeGraph(type) {
+    if (type == 'graph1') {
+      this.graph1 = true;
+      this.graph2 = false;
+      this.graph3 = false;
+    } else if (type == 'graph2') {
+      this.graph1 = false;
+      this.graph2 = true;
+      this.graph3 = false;
+    } else if (type == 'graph3') {
+      this.graph1 = false;
+      this.graph2 = false;
+      this.graph3 = true;
+    }
+  }
+
+public data = {
+    simpleLineOptions: {
+      color: '#FFFFF',
+      fullWidth: true,
+      height: '300px',
+      chartPadding: {
+        right: 40
+      }
+    },
+    simpleLineData: {
+      labels: ['Mon', 'Tue', 'Wed', 'Thu', 'Fri'],
+      series: [
+        [20, 20, 12, 45, 50],
+        [10, 45, 30, 14, 12],
+        [34, 12, 12, 40, 50],
+        [10, 43, 25, 22, 16],
+        [3, 6, 30, 33, 43]
+      ]
+    },
+    areaLineData: {
+      labels: [1, 2, 3, 4, 5, 6, 7, 8],
+      series: [
+        [5, 9, 7, 8, 5, 3, 5, 4]
+      ]
+    },
+    areaLineOptions: {
+      fullWidth: true,
+      height: '300px',
+      low: 0,
+      showArea: true
+    },
+    biLineData: {
+      labels: ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun'],
+      series: [
+        [1, 2, 3, 1, -2, 0, 1],
+        [-2, -1, -2, -1, -2.5, -1, -2],
+        [0, 0, 0, 1, 2, 2.5, 2],
+        [2.5, 2, 1, 0.5, 1, 0.5, -1]
+      ]
+    },
+
+    biLineOptions: {
+      height: '300px',
+      high: 3,
+      low: -3,
+      showArea: true,
+      showLine: false,
+      showPoint: false,
+      fullWidth: true,
+      axisX: {
+        showGrid: false
+      }
+    },
+    simpleBarData: {
+      labels: ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'],
+      series: [
+        [15, 24, 43, 27, 5, 10, 23, 44, 68, 50, 26, 8],
+        [13, 22, 49, 22, 4, 6, 24, 46, 57, 48, 22, 4]
+      ]
+    },
+    simpleBarOptions: {
+      fullWidth: true,
+      height: '300px'
+    },
+    multiBarData: {
+      labels: ['Quarter 1', 'Quarter 2', 'Quarter 3', 'Quarter 4'],
+      series: [
+        [5, 4, 3, 7],
+        [3, 2, 9, 5],
+        [1, 5, 8, 4],
+        [2, 3, 4, 6],
+        [4, 1, 2, 1]
+      ]
+    },
+    multiBarOptions: {
+      fullWidth: true,
+      height: '300px',
+      stackBars: true,
+      axisX: {
+        labelInterpolationFnc: function (value) {
+          return value.split(/\s+/).map(function (word) {
+            return word[0];
+          }).join('');
+        }
+      },
+      axisY: {
+        offset: 20
+      }
+    },
+    multiBarResponsive: [
+      ['screen and (min-width: 400px)', {
+        reverseData: true,
+        horizontalBars: true,
+        axisX: {
+          labelInterpolationFnc: (n) => n
+        },
+        axisY: {
+          offset: 60
+        }
+      }],
+      ['screen and (min-width: 700px)', {
+        stackBars: false,
+        reverseData: false,
+        horizontalBars: false,
+        seriesBarDistance: 15
+      }]
+    ],
+    stackedBarData: {
+      labels: ['Quarter 1', 'Quarter 2', 'Quarter 3', 'Quarter 4'],
+      series: [
+        [800000, 1200000, 1400000, 1300000],
+        [200000, 400000, 500000, 300000],
+        [100000, 200000, 400000, 600000]
+      ]
+    },
+    stackedBarOptions: {
+      fullWidth: true,
+      height: '300px',
+      stackBars: true,
+      axisY: {
+        labelInterpolationFnc: function (value) {
+          return (value / 1000) + 'k';
+        }
+      }
+    },
+    simplePieData: {
+      series: [5, 3, 4]
+    },
+    simplePieOptions: {
+      fullWidth: true,
+      height: '300px',
+      weight: '300px',
+      labelInterpolationFnc: function (value) {
+        return Math.round(value / 12 * 100) + '%';
+      }
+    },
+    labelsPieData: {
+      labels: ['Bananas', 'Apples', 'Grapes'],
+      series: [20, 15, 40]
+    },
+    labelsPieOptions: {
+      fullWidth: true,
+      height: '300px',
+      weight: '300px',
+      labelDirection: 'explode',
+      labelInterpolationFnc: function (value) {
+        return value[0];
+      }
+    },
+    simpleDonutData: {
+      labels: ['Bananas', 'Apples', 'Grapes'],
+      series: [20, 15, 40]
+    },
+    simpleDonutOptions: {
+      fullWidth: true,
+      donut: true,
+      height: '300px',
+      weight: '300px',
+      labelDirection: 'explode',
+      labelInterpolationFnc: function (value) {
+        return value[0];
+      }
+    }
+  };
+
+  constructor(af: AngularFire) {
+    this._af = af;
+  }
+
+  ngOnInit() {
+      this._af.database.list('/data', { preserveSnapshot: true})
+        .subscribe(snapshots => {
+          snapshots.forEach(snapshot => {
+            console.log(snapshot.key);
+            console.log(snapshot.val());
+          })
+        })
   }
 
 }
